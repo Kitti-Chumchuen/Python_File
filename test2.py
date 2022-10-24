5c0cd0ae2350c6f154edbacabf0255c5b59ef8b3
64010051 <64010051@kmitl.ac.th>
2022-10-25 01:14:24 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,44 +1,8 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-    
-    def __str__(self):
-        return str(self.data)
-
-class BST:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, data):
-        nn = Node(data)
-        if self.root != None:
-            if data < self.root:
-                if self.root.left:
-                    nn.left = self.root.left
-                    self.root.left.left = nn
-                else:
-                    self.root.left = nn
-            elif data > self.root:
-                if self.root.right:
-                    nn.right = self.root.right
-                    self.root.right.right = nn
-                else:
-                    self.root.right = nn
-        else:
-            self.root = data
-        print(data)
-
-    
-    def printTree(self, node, level = 0):
-        if node != None:
-            self.printTree(node.right, level + 1)
-            print('     ' * level, node)
-            self.printTree(node.left, level + 1)
-
-T = BST()
-inp = [int(i) for i in input('Enter Input : ').split()]
+inp = input('Enter Input : ').split(",")
+opt, num, n = "", [], 0
 for i in inp:
-    root = T.insert(i)
-T.printTree(root)
+    a = i.split(" ")
+    opt += a[0]
+    num[n] = a[1]
+    n += 1
+print(opt, num)
