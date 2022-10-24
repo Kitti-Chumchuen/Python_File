2ac4b3735e1cfdd71882f9f180b33d3cefe1e7bf
64010051 <64010051@kmitl.ac.th>
2022-10-25 01:14:21 +0700

    Create LAB8_1.py

---
+++ LAB8_1.py
@@ -0,0 +1,12 @@
+class Node:
+
+    def __init__ (self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+        self.height = 1
+
+class AVL_Tree:
+
+     def __init__ (self, node, item):
+        
