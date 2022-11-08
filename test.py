fd2b382196352911bae20d5df27e51a7a0b21369
64010051 <64010051@kmitl.ac.th>
2022-11-09 00:34:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,53 @@
-def print1ToN(n) :
-    if n > 1 :
-        print1ToN(n-1)
-        print(n , end = " ")
-    elif n == 0 :
-        print("1" , end = " ")
-    else :
-        print("1" , end = " ")
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
 
-def printNto1(n) :
-    if n > 1 :
-        print(n , end = " ")
-        printNto1(n-1)
-    elif n == 0 :
-        print("1" , end = " ")
-    else :
-        print("1" , end = " ")
+class BST:
+    def __init__(self):
+        self.root = None
 
-n = int(input("Enter Input : "))
-print1ToN(n)
-printNto1(n)
+    def insert(self, data):  
+        if self.root == None:
+            nn = Node(data)
+            self.root = nn
+        else:
+            cur = self.root
+            while cur:
+                if data > cur.data:
+                    if cur.right == None:
+                        nn = Node(data)
+                        cur.right = nn
+                        break
+                    else:
+                        cur = cur.right
+                else:
+                    if cur.left == None:
+                        nn = Node(data)
+                        cur.left = nn
+                        break
+                    else:
+                        cur = cur.left
+        return self.root
+    
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+T = BST()
+inp = input('Enter Input : ').split(" ")
+a = inp[-1].split("/")
+inp[-1] = a[0]
+n = int(a[1])
+
+print(n)
+for i in inp:
+    root = T.insert(int(i))
+    T.printTree(root,0)
+    print("--------------------------------------------------")
